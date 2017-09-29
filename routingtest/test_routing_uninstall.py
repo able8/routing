f6afe53941fc317afd71ccd106f9cfc178ca4158
peter.pan <peter.pan@gnomito.com>
2017-09-29 10:30:46 +0800

    change comments and delete unnecessary test

--- routingtest/test_routing_uninstall.py
+++ routingtest/test_routing_uninstall.py
@@ -13,76 +13,52 @@ LAN = 'br0'
 #       NOTExist( /etc/network/if.backup )
 #       Exist( /etc/network/interfaces ), NOTEqual( /etc/network/interfaces, ~/Bay-Net-Client/routing/interfaces )
 #       NOTExist( /etc/routing )
-#       NOTExist( /etc/routing/routing.sh ), NOTEqual( /etc/routing/routing.sh, ~/Bay-Net-Client/routing/routing.sh )
-#       NOTExist( /etc/systemd/system/bay_routing.service ), NOTEqual( /etc/systemd/system/bay_routing.service, ~/Bay-Net-Client/routing/bay_routing.service)
+#       NOTExist( /etc/routing/routing.sh )
+#       NOTExist( /etc/systemd/system/bay_routing.service )
 #
-# 2. Interfaces Informations
-#       Match( GetIP( enp2s0 ), 192.168.*.* ), Match( GetMask( enp2s0 ), 255.255.255.0 )
-#       NOTMatch( GetIP( br0 ), 192.168.0.1 ), NOTMatch( GetMask( br0 ), 255.255.254.0 )
-#
-# 3. Service
+# 2. Service
 #       NOTMatch( GetServiceStatus( bay_routing.service ), active )
 #
-# 4. Iptables
+# 3. Iptables
 #       NOTMatch( GetIptableRule( nat, POSTROUTING, WAN ), MASQUERADE*WAN )
 #       NOTMatch( GetIptableRule( filter, FORWARD, WAN ), ACCEPTWANLANACCEPTLANWAN )
 
 class test_Config_Files_and_Directories(unittest.TestCase):
     '''test whether config files and directories exist or equal'''
     def test_if_backup(self):
-        '''test whether if.backup exists'''
+        '''test whether if.backup NOT exists'''
         self.assertFalse( Exist( '/etc/network/if.backup' ) )
 
     def test_interfaces(self):
-        '''test whether /etc/network/interfaces exists and is equal with /home/murka/Bay-Net-Client/routing/interfaces'''
+        '''test whether /etc/network/interfaces exists and is NOT equal with /home/murka/Bay-Net-Client/routing/interfaces'''
         self.assertTrue( Exist( '/etc/network/interfaces' ) )
         self.assertFalse( Equal( '/etc/network/interfaces', '/home/murka/Bay-Net-Client/routing/interfaces' ) )
     
     def test_routing_dir(self):    
-        '''test /etc/routing directory exists'''
+        '''test /etc/routing directory NOT exists'''
         self.assertFalse( Exist( '/etc/routing' ) )
     
     def test_routing_sh(self):
-        '''test whether /etc/routing/routing.sh exists and is equal with ~/Bay-Net-Client/routing/routing.sh'''
+        '''test whether /etc/routing/routing.sh NOT exists'''
         self.assertFalse( Exist( '/etc/routing/routing.sh' ) )
-        self.assertFalse( Equal( '/etc/routing/routing.sh', '/home/murka/Bay-Net-Client/routing/routing.sh' ) )
 
     def test_bay_routing_service(self):
-        '''test whether /etc/systemd/system/bay_routing.service exists and is equal with ~/Bay-Net-Client/routing/bay_routing.service'''
+        '''test whether /etc/systemd/system/bay_routing.service NOT exists'''
         self.assertFalse( Exist( '/etc/systemd/system/bay_routing.service' ) )
-        self.assertFalse( Equal( '/etc/systemd/system/bay_routing.service', '/home/murka/Bay-Net-Client/routing/bay_routing.service' ) )
-
-class test_Interfaces_Information( unittest.TestCase ):
-    '''test interfaces '''
-    def test_WAN_IP( self ):
-        '''test whether WAN IP is 192.168.xxx.xxx'''
-#    self.assertTrue( Match( GetIP( WAN ), 192.168.[0-9] ) )
-
-    def test_WAN_netmask( self ):
-        '''test whether WAN netmask is 255.255.255.0'''
-        self.assertTrue( Match( GetMask( WAN ), '255.255.255.0' ) )
-
-    def test_LAN_IP( self ):
-        '''test whether LAN IP is 192.168.0.1'''
-        self.assertFalse( Match( GetIP( LAN ), '192.168.0.1' ) )
-
-    def test_LAN_natmask( self ):
-        '''test whether LAN netmask is 255.255.254.0'''
-        self.assertFalse( Match( GetMask( LAN ), '255.255.254.0' ) )
 
 class test_service( unittest.TestCase ):
     def test_service_is_active( self ):
-        '''test whether bay_routing.service is active'''    
+        '''test whether bay_routing.service is NOT active'''    
         self.assertFalse( Match( GetServiceStatus( 'bay_routing.service' ), 'active' ) )    
 
 class test_Iptables( unittest.TestCase ):
     '''test whether iptables rules exist'''
     def test_nat_POSTROUTING_MASQUERADE( self ):
-        '''test whether MASQUERADE rules exist'''
+        '''test whether MASQUERADE rules NOT exist'''
         self.assertFalse( Match( GetIptableRule( 'nat', 'POSTROUTING', WAN  ), 'MASQUERADE*%s' % ( WAN ) ) )
 
     def test_filter_FORWARD( self ):
-        '''test whether LAN and WAN data can exchange'''
+        '''test whether LAN and WAN data can NOT exchange'''
         self.assertFalse( Match( GetIptableRule( 'filter', 'FORWARD', WAN ), 'ACCEPT%s%sACCEPT%s%s' % ( WAN, LAN, LAN, WAN ) ) )
 
 
